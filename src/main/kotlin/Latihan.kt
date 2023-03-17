228bacfe849940996969266fc0174019b4b67a62
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:37:32 +0700

    Latihan BP2

--- src/main/kotlin/Latihan.kt
+++ src/main/kotlin/Latihan.kt
@@ -1,13 +1,22 @@
 
 fun main() {
-    println("Hello world!")
-    println("Ibrahim Mufti")
+//    println("Hello world!")
+//    println("Ibrahim Mufti")
+//
+//    print ("Pekerjaan : ")
+//    val Pekerjaan = readLine()
+//
+//    print ("Alamat : ")
+//    val Alamat = readLine()
+//
+//    println("Pekerjaan kamu adalah $Pekerjaan dan alamat kamu di $Alamat")
 
-    print ("Pekerjaan : ")
-    val Pekerjaan = readLine()
-
-    print ("Alamat : ")
-    val Alamat = readLine()
-
-    println("Pekerjaan kamu adalah $Pekerjaan dan alamat kamu di $Alamat")
+    //Pertemuan 2
+    var namaLengkap: String= "Ibrahim"
+    var alamat: String
+    alamat = "Yogyakarta"
+    var umur = 18
+    println(namaLengkap)
+    println(alamat)
+    println(umur)
 }
